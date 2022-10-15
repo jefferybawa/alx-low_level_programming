90ad75efe75345461b0cba12e77f699f39ac8241
jefferybawa <jefferybawaemuyahe@gmail.com>
2022-10-15 13:37:36 -0700

    gratter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ int main(void)
 		{
 			printf("last digit of %d is %d and is greater than 5\n", n, a);
 		}
-		else if (a < 6 && a != 0)
+		else if (a == 0)
 		{
 			printf("last digit of %d is %d and is less than 6 and not 0\n", n, a);
 		}
