62a9dc47fa03f84548d8430c902194816ddad08e
jefferybawa <jefferybawaemuyahe@gmail.com>
2022-10-17 20:32:06 -0700

    gg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,21 +8,23 @@ int main(void)
 {
 	int n;
 
-	for (n = 48; n < 58; n++)
+	for (n = 0; n <= 9; n++)
 	{
 
-		putchar (n);
-		if (n != 57)
+		putchar ('0' + n);
+		if (n == 9)
 		{
-			putchar(',');
-			putchar(' ');
+			brake;
 		}
-
-		putchar ('\n');
+		else
+		{
+			putchar(',')
+			putchar(' ')
+	}
 	}
 
+	putchar('\n');
+
 	return (0);
-
 }
-
 
