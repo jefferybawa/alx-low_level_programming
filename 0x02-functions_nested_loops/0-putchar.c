ed07aa10c8ce591d84949695c5d2f9ab2ce6e2fb
jefferybawa <jefferybawaemuyahe@gmail.com>
2022-10-19 10:29:38 -0700

    print putchsr

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - print putchar on a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	_putchar(',');
+	_putchar('p');
+	_putchar('U');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
