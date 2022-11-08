3ec559dd3994f0d1a6f1256b41f3570d87946fc4
jefferybawa <jefferybawaemuyahe@gmail.com>
2022-11-07 16:59:50 -0800

    arrays

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * create_array -arrays for print a string
+ * @size: number elements array
+ * @c: char
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *buffer;
+	unsigned int position;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	/*Define value with malloc*/
+	buffer = (char *)malloc(size * size of(c));
+
+	if (buffer == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		position = 0;
+		while (position < size) /*while for array */
+		{
+			(buffer + position) = c;
+			position++;
+		}
+		return (buffer);
+	}
+}
---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - write the character c to stdout
+ * @c:The character to print
+ * Return: on success 1
+ * on error, -1 is returned,and error is set approprately
+ */
+int _putchar(char c)
+{
+	return(write(1 &c, 1));
+}
