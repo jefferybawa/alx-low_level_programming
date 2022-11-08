fe62f371887469c85b627d8c98fde4f0d5b00e4f
jefferybawa <jefferybawaemuyahe@gmail.com>
2022-11-07 17:24:14 -0800

    create arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -28,7 +28,7 @@ char *create_array(unsigned int size, char c)
 		position = 0;
 		while (position < size) /*while for array */
 		{
-			(buffer + position) = c;
+			*(buffer + position) = c;
 			position++;
 		}
 		return (buffer);
